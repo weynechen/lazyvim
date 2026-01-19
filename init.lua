afc99165162d2517584c09a7221f330f0a88d8fa
weynechen <weynechen@qq.com>
2026-01-19 23:09:11 +0800

    add cmake

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+--vim.env.http_proxy = "socks5://127.0.0.1:10803"
+--vim.env.https_proxy = "socks5://127.0.0.1:10803"
+--vim.env.ALL_PROXY = "socks5://127.0.0.1:10803"
