3eb8be8084ea8c79f159500ac43975522b979ced
weynechen <weynechen@qq.com>
2026-01-20 16:54:10 +0800

    set tab and indentation settigns

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-vim.env.http_proxy = "socks5://127.0.0.1:10803"
-vim.env.https_proxy = "socks5://127.0.0.1:10803"
-vim.env.ALL_PROXY = "socks5://127.0.0.1:10803"
+-- vim.env.http_proxy = "socks5://127.0.0.1:10803"
+-- vim.env.https_proxy = "socks5://127.0.0.1:10803"
+-- vim.env.ALL_PROXY = "socks5://127.0.0.1:10803"
 require("config.lazy")
-
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,10 @@
 
 vim.g.lazyvim_colorscheme = "cursor-dark"
 vim.g.lazyvim_check_order = false
+vim.g.autoformat = false
+
+-- Set tab and indentation settings
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
