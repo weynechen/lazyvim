b8a86590ffc145f5c74740527abd608c6a037d0d
weynechen <weynechen@qq.com>
2026-01-20 22:04:01 +0800

    put a cat on dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,1 +1,20 @@
-return {}
+return {
+  -- Customize snacks.nvim dashboard
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+ _._     _,-'""`-._
+(,-.`._,'(       |\`-/|
+    `-.-' \ )-`( , o o)
+          `-    \`_`"'-
+        
+         Hi    Weyne 
+]],
+        },
+      },
+    },
+  },
+}
