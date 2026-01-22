eb3e24d6f448d9f10ade0509f3b25020db3fabcd
weynechen <weynechen@qq.com>
2026-01-22 23:16:27 +0800

    try vscode theme

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.lazyvim_colorscheme = "cursor-dark"
+vim.g.lazyvim_colorscheme = "vscode"
 vim.g.lazyvim_check_order = false
 vim.g.autoformat = false
 
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,20 @@
 return {
   {
+    "Mofiqul/vscode.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "dark",
+      transparent = false,
+      italic_comments = true, -- Apply theme colors to terminal
+      terminal_colors = true,
+    },
+    config = function(_, opts)
+      require("vscode").setup(opts)
+      require("vscode").load()
+    end,
+  },
+  {
     "ydkulks/cursor-dark.nvim",
     lazy = false,
     priority = 1000,
