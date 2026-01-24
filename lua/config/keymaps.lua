bb279387d2fce6fecbaa861ef33ef9451f5c182d
weynechen <weynechen@qq.com>
2026-01-24 22:23:12 +0800

    Assign a key binding to toggle dashboard and reduce the width of explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open dashboard
+vim.keymap.set("n", "<leader>h", function()
+  require("snacks").dashboard()
+end, { desc = "Dashboard" })
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -15,6 +15,23 @@ return {
 ]],
         },
       },
+      -- Enable image support
+      image = {
+        enabled = true,
+      },
+      -- Configure picker explorer layout width
+      picker = {
+        sources = {
+          explorer = {
+            layout = {
+              layout = {
+                width = 30, -- Fixed width in columns (default: 40, adjust as needed)
+                -- width = 0.2, -- Or use relative width (20% of screen width)
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
